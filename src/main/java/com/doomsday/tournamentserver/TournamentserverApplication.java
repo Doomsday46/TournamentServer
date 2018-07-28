44f88540544bb7d854cda1fd75d0819cbb5a784c
Vitaliy <Gallifrey46@gmail.com>
2018-07-28 15:28:25 +0300

    Добавление spring проекта

---
+++ src/main/java/com/doomsday/tournamentserver/TournamentserverApplication.java
@@ -0,0 +1,12 @@
+package com.doomsday.tournamentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentserverApplication.class, args);
+	}
+}
