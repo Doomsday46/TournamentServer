1ae3107bea7c3a80be4602233ed358b793351272
Vitaliy <Gallifrey46@gmail.com>
2018-07-30 12:24:59 +0300

    Реализована жеребьевка случайным образом

--- src/main/java/com/doomsday/tournamentserver/domain/tossup/RandomTossUp.java
+++ src/main/java/com/doomsday/tournamentserver/domain/tossup/RandomTossUp.java
@@ -1,9 +1,9 @@
 package com.doomsday.tournamentserver.domain.tossup;
 
 import com.doomsday.tournamentserver.domain.pair.PairNumberofPlayer;
-import com.doomsday.tournamentserver.service.PlayerService;
-import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class RandomTossUp implements TossUp{
@@ -12,13 +12,24 @@ public class RandomTossUp implements TossUp{
     private List<Integer> numbersPlayers;
 
     public RandomTossUp(Integer countPlayers){
-        if(countPlayers < 2 ) throw new IllegalArgumentException();
+        if(countPlayers < 2 )  throw new IllegalArgumentException();
         this.countPlayers = countPlayers;
     }
 
     @Override
-    public List<PairNumberofPlayer> getPairsPlayers() {
-
-        return null;
+    public List<Integer> getPairsPlayers() {
+        numbersPlayers = initListNumbersPlayers();
+        shuffle();
+        return numbersPlayers;
+    }
+        
+    private List<Integer> initListNumbersPlayers(){
+        List<Integer> numbers = new ArrayList<>();
+        for(int i=1; i<=countPlayers; i++)
+            numbers.add(i);
+        return numbers;
+    }
+    private void shuffle(){
+        Collections.shuffle(numbersPlayers);
     }
 }
